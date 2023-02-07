7a8a2edf729608c1dd77312b1eca35b418e8583b
MyUserKapilash <124327889+MyUserKapilash@users.noreply.github.com>
2023-02-07 12:33:57 +0530

    Add files via upload

---
+++ kkkkkk_ow.py
@@ -0,0 +1,20 @@
+#print("hello world")
+name = "Ram"
+age = 22
+print(name,age)
+print("address of name: ",id(name))
+print("address of age: ",id(age))
+name = "john"
+age = 23
+print(name,age)
+print("address of name: ",id(name))
+print("address of age: ",id(age))
+a = 100
+A = 200
+print(a+A)
+print(name,age)
+
+name = "john"
+age = 23
+print(name,age,"hello world",sep="*")
+print("bengalore")
---
+++ python_bang.py
@@ -0,0 +1,5 @@
+if 100 > 99and \
+200 <= 300 and \
+True != False:
+    print('Hello Wold!')
+   

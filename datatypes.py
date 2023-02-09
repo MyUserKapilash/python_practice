3537e8235db7f8d3c24022fd3b5ff712548383ea
MyUserKapilash <124327889+MyUserKapilash@users.noreply.github.com>
2023-02-09 09:00:29 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+age = 22
+print("Datatype of age:",type(age))
+
+pi = 3.14
+print("Datatype of pi:",(pi))
+
+st_pass = True
+print("Datatype of st_pass:",(st_pass))
+
+name = "Ram"
+print("Datatype of name:",type(name))
+
+c_num = 6+6j
+print("Datatype of c_num:",(c_num))
+
+print(bool(0))
+print(bool(""))
+print(bool(" "))
+print(bool("-90"))
+
+
+

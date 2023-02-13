5970b395461b59acfa162ab326fbfef12cd4cfc0
MyUserKapilash <124327889+MyUserKapilash@users.noreply.github.com>
2023-02-13 09:06:22 +0530

    Add files via upload

---
+++ 13-02_Indexing.py
@@ -0,0 +1,17 @@
+#city="Bengaluru"
+#print("city:",(city))
+#print(city[0])
+
+#print(city[3])
+
+#print("length of city:",len(city))
+
+#print(city[-4])
+
+#print(city[0:4])
+
+city="Bengaluru"
+print("city:",(city))
+print(city[0:7:2])
+print(city[0:4:6])
+
---
+++ 13-02_Non_primitive.py
@@ -0,0 +1,17 @@
+#list
+#l=[1,2,3,True,"pune",3.14]
+#print(l)
+#print(type(l))
+
+
+#l1=list()
+#print(l1)
+
+#Range
+#l2=list(range(1,11))
+#print(l2)
+
+l3=list(range(0,11,4))
+print(l3)
+
+
